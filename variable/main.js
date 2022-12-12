ef5d1b6404d2a05ae612b5686390487d1f2faf86
kavi307 <ajkavitha7@gmail.com>
2022-12-12 19:01:03 +0530

    variable task

---
+++ variable/main.js
@@ -0,0 +1,49 @@
+var num=20;
+console.log(num);
+console.log(typeof (num));
+
+let fname="kavi";
+console.log(fname);
+console.log(typeof (fname));
+
+let lname="A";
+console.log(lname);
+console.log(typeof (lname));
+
+ var young=true;
+ console.log(young);
+ console.log(typeof(young));
+
+ var young=false;
+ console.log(young);
+ console.log(typeof(young));
+
+
+ var place=null;
+    console.log(place);
+    console.log(typeof(place));
+
+
+var number=1232378787287528175372n;
+    console.log(number);
+    console.log(typeof(number));
+
+
+var defined;
+    console.log(defined);
+
+
+
+    var a=10;
+    console.log(++a);
+    
+
+    var b=12;
+    console.log(b++);
+    console.log(b);
+
+    
+    var mark=35;
+
+    var code=(mark>=35)?'pass':'fail';
+    console.log(code);
