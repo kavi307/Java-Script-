df9be805872bea3b4250d792c082e77219339f48
kavi307 <ajkavitha7@gmail.com>
2022-12-13 16:39:17 +0530

    condition task

---
+++ condition/main.js
@@ -0,0 +1,11 @@
+alert("Hiii kavii")
+var a=prompt("enter the number!");
+    if(a>0){
+        console.log(a+"this possitive number");
+    }
+    else if(a<0){
+        console.log(a+"This negative number");
+    }
+    else{
+        console.log(a+"this zero value");
+    }
