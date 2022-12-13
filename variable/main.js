367daaaa5bb471d7aeec6f72bdffb705378ab158
kavi307 <ajkavitha7@gmail.com>
2022-12-13 16:37:56 +0530

    variable task

--- variable/main.js
+++ variable/main.js
@@ -1,3 +1,4 @@
+                    /*
 var num=20;
 console.log(num);
 console.log(typeof (num));
@@ -47,3 +48,17 @@ var defined;
 
     var code=(mark>=35)?'pass':'fail';
     console.log(code);
+                        
+        var a,b,c;
+        a=prompt(parseInt("enter the radius"));
+        b=22/7;
+        c=b*a*a;
+        console.log(c);
+                        */
+
+        var a,b,h,traingle;
+        a=0.5;
+        b=prompt(parseInt("enter the value"));
+        h=prompt(parseInt("enter the radius"));
+        traingle=a*b*h;
+        console.log(traingle);
